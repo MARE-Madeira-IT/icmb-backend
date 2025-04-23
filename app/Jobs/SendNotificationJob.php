a22809f04d206d87b40d74a4fa3f5cfcf6d60c93
m3ida <andre.almeida@mare.arditi.pt>
2025-04-23 12:52:49 +0100

    removed guzzle test and changed firebase job

--- app/Jobs/SendNotificationJob.php
+++ app/Jobs/SendNotificationJob.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Exception\ConnectException;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Factory;
 
 class SendNotificationJob implements ShouldQueue
 {
@@ -28,7 +29,9 @@ class SendNotificationJob implements ShouldQueue
     public function handle(): void
     {
         try {
-            $messaging = app('firebase.messaging');
+
+            $factory = (new Factory)->withServiceAccount('/storage/app/private/serviceAccountKey.json');
+            $messaging = $factory->createMessaging();
 
             $message = CloudMessage::new()
                 // ->withNotification([
@@ -39,7 +42,7 @@ class SendNotificationJob implements ShouldQueue
 
             $tokens = $this->user->pushNotificationTokens()->pluck("token")->toArray();
 
-            $messaging->sendMulticast($message, $tokens);
+            $messaging->sendMulticast($message, ['dNb3uuHuVkSbtRLb3xJ2X2:APA91bFhWPT6lu_tp8tRSU5b3siw7I0BR_PD75z70jnC6ThYUZeqvk2tMoiKKeAU3vBcSGvCgwgP3ohVTuaAzlab55hVhcIp35dBPsLfO2F6sY1Zvs4ZcN4']);
         } catch (ConnectException $th) {
             logger("CAUGHT!");
             logger($th->getMessage());
